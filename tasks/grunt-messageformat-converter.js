8fc08fdde31a96780653f982dbca791268c82526
Eli Mallon <eli.mallon@realpage.com>
2014-11-03 11:03:03 -0800

    fix "from" and "to"

--- tasks/grunt-messageformat-converter.js
+++ tasks/grunt-messageformat-converter.js
@@ -4,18 +4,26 @@ var mconv = require('messageformat-converter');
 
 module.exports = function(grunt) {
 
-    grunt.registerMultiTask('messageformatConverter', 'change translation formats', function(){
+    grunt.registerMultiTask('messageformat-converter', 'change translation formats', function(){
+
+        var self = this;
 
         grunt.verbose.writeln('converting ' + grunt.log.wordlist(this.files.map(function(file){
             return file.src;
         })));
 
-        grunt.verbose.or.writeln('whatevering ' + this.files.length + ' file(s)');
+        grunt.verbose.or.writeln('converting ' + this.files.length + ' file(s)');
 
         this.files.forEach(function(file){
-            var contents = grunt.file.read(file.src);
-            var output = mconv.convertFile(contents).from(this.options('from')).to(this.options('to'));
-            grunt.file.write(file.dest, output);
+            try {
+                var contents = grunt.file.read(file.src);
+                var output = mconv.convertFile(contents).from(self.options().from).to(self.options().to);
+                grunt.file.write(file.dest, output);
+            }
+            catch (e) {
+                console.error(e.stack);
+                throw e;
+            }
         });
 
     });
